44e8c0cfa73d6363b701c4fa347b4cf919020b9c
marwansultan97 <marwansultan97@gmail.com>
2021-11-03 15:44:36 +0200

    Initial Commit

---
+++ Onboarding-SwiftUI/Onboarding_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onboarding_SwiftUIApp.swift
+//  Onboarding-SwiftUI
+//
+//  Created by Marwan Osama on 03/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Onboarding_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
